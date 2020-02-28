f48fd348a1bc9f30aae1d697c3dcdf550985f550
Geoff Kendal <gkendal@williamhill.co.uk>
2020-02-28 17:49:46 +0000

    :monkey_face: Create own VPC, so we can reapply terraform if ChaosMonkey causes problems

--- deploy.tf
+++ deploy.tf
@@ -3,8 +3,20 @@ provider "aws" {
   profile = "default"
 }
 
+module "vpc" {
+  source         = "terraform-aws-modules/vpc/aws"
+  name           = "unicorn-vpc"
+  cidr           = "10.0.0.0/16"
+  azs            = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.0.21.0/24", "10.0.22.0/24"]
+
+  public_dedicated_network_acl = true
+  enable_dns_hostnames         = true
+}
+
 resource "aws_security_group" "unicorn-sg" {
-  name = "unicorn-sg"
+  name   = "unicorn-sg"
+  vpc_id = module.vpc.vpc_id
 
   ingress {
     cidr_blocks = [
@@ -49,24 +61,35 @@ resource "aws_key_pair" "ssh-key" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCE2B/Ajo0MB75CtKS14fVAsPN+qTF10v7BKNNwYAdCHT8N+diqEUQltEbBOkdiRaD8+ro4fZQuxYhnJHJKjXfVxwdBU9/yPN/FszVNCSxPgIgBEJ0AocZ3cwEAruTWH4SHhVgmpVhPoBOl+FvR9O2k79SFKO7msOPHt58HghZmRhwBhv65ixIvK9RhbzZPmdPMjDRpvDotvD0ffUNd5GkhWi/JOef2zBTZRKjwsUUYMD3Z76BYLPQFbpKUv2O72BLHM0xcoZG3LubrSOUbKh+G/QosZDy4PBRIC3Ld/WyZgScjGI4/ccVjA5dV+Oq08+3b8Nj1M6lXF2yhqxgRWV1/"
 }
 
-resource "aws_instance" "app-instance" {
-  ami           = "ami-07d0cf3af28718ef8"
-  instance_type = "t2.micro"
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners = ["amazon"]
 
-  security_groups = ["${aws_security_group.unicorn-sg.name}"]
-  key_name        = "${aws_key_pair.ssh-key.key_name}"
-
-  tags = {
-    Name = "Unicorn-WS-0"
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
   }
 }
+
+resource "aws_instance" "app-instance" {
+  instance_type   = "t2.micro"
+  ami             = data.aws_ami.amazon-linux-2.id
+  security_groups = ["${aws_security_group.unicorn-sg.id}"]
+  key_name        = aws_key_pair.ssh-key.key_name
+  subnet_id       = module.vpc.public_subnets[0]
+
+  tags = {
+    Name = "Unicorn-app"
+  }
+}
+
 
 resource "aws_cloudfront_distribution" "app-cache" {
   enabled = true
 
   origin {
-    domain_name = "${aws_instance.app-instance.public_dns}"
-    origin_id   = "${aws_instance.app-instance.id}"
+    domain_name = aws_instance.app-instance.public_dns
+    origin_id   = aws_instance.app-instance.id
 
     custom_origin_config {
       http_port              = 80
@@ -81,7 +104,7 @@ resource "aws_cloudfront_distribution" "app-cache" {
     cached_methods         = ["HEAD", "GET", "OPTIONS"]
     viewer_protocol_policy = "redirect-to-https"
 
-    target_origin_id = "${aws_instance.app-instance.id}"
+    target_origin_id = aws_instance.app-instance.id
 
     forwarded_values {
       query_string = true
@@ -142,4 +165,8 @@ resource "aws_cloudfront_distribution" "app-cache" {
     cloudfront_default_certificate = true
   }
 
+}
+
+output "cloudfront_doamin" {
+  value       = aws_cloudfront_distribution.app-cache.domain_name
 }
